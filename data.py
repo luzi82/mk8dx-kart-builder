338f2a2dd0458baaf53a9d98f3fdf024f3d04f18
Luzi82 <luzi82@gmail.com>
2017-05-13 17:53:49 +0800

    group parts having same stat

--- data.py
+++ data.py
@@ -33,6 +33,33 @@ for stat_type in stat_type_list:
     for stat in stat_list:
         stat[stid] = round(stat[stid])
 
+# Group same stat part
+part_group_list = []
+for stat in stat_list:
+    if 'group' in stat:
+        continue
+    part_group_id = "group"+stat['id']
+    part_group = {stat_type['id']:stat[stat_type['id']] for stat_type in stat_type_list}
+    part_group['id'] = part_group_id
+    part_group['type'] = stat['type']
+    part_group['member'] = [stat['id']]
+    part_group_list.append(part_group)
+    
+    stat['group'] = part_group_id
+    for stat0 in stat_list:
+        if 'group' in stat0:
+            continue
+        if stat0['type']!=part_group['type']:
+            continue
+        good = True
+        for stat_type in stat_type_list:
+            stat_id = stat_type['id']
+            good = good and (stat0[stat_id] == part_group[stat_id])
+        if not good:
+            continue
+        stat0['group'] = part_group_id
+        part_group['member'].append(stat0['id'])
+
 # Get the min max of each stat in each parts
 for stat_type in stat_type_list:
     if stat_type['type'] != 'v100':
@@ -58,10 +85,15 @@ for stat_type in stat_type_list:
 stat_min = min([stat_type['min'] for stat_type in stat_type_list if stat_type['type'] == 'v100'])
 stat_max = max([stat_type['max'] for stat_type in stat_type_list if stat_type['type'] == 'v100'])
 
-# Group parts into different list
+# Group parts into different list by type
 part_dict = {part_type['id']:[] for part_type in part_type_list}
 for stat in stat_list:
     part_dict[stat['type']].append(stat)
+
+# Group part group into different list by type
+part_group_dict = {part_type['id']:[] for part_type in part_type_list}
+for part_group in part_group_list:
+    part_group_dict[part_group['type']].append(part_group)
 
 output = {
     "stat_type_list":stat_type_list,
@@ -70,6 +102,7 @@ output = {
     "stat_max":stat_max,
     "part_dict":part_dict,
     "part_type_list":part_type_list,
+    "part_group_dict":part_group_dict,
 }
 
 output = json.dumps(output,sort_keys=True,indent=2)
