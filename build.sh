59b57c08fcdd0ada60bf496c52c82d5e4d172361
Luzi82 <luzi82@gmail.com>
2017-05-13 22:15:42 +0800

    fix build script

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 rm -rf output
 mkdir output
 
+python3 data.py > data.json
+php kartbuilder.php > kartbuilder.html
+
 cp data.json output/
 cp jquery-3.2.1.min.js output/
 cp kartbuilder.html output/
