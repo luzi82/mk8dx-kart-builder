3ebcba5ebbb90b0e621571b4ddc6e4a7b340d681
Luzi82 <luzi82@gmail.com>
2017-06-07 00:52:12 +0800

    update comment

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ python3 data.py > data.json
 php kartbuilder.php > kartbuilder.html
 
 echo \{\"build\":\"${NOW}\",\"version\":\"${GITHASH}\"\} >> output/version.json
+cp stat.csv output/
 cp data.json output/
 cp jquery-3.2.1.min.js output/
 cp kartbuilder.html output/
